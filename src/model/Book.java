6b68c35add7dd89baa9fae21c440236bbdf108a6
emilinsuresh <emilinsuresh@gmail.com>
2025-10-03 03:08:37 +0530

    Create Book.java
    Added Book class to model package

---
+++ src/model/Book.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+
+    public Book() {}
+
+    public Book(int id, String title, String author, String category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    @Override
+    public String toString() {
+        return "Book{id=" + id + ", title='" + title + "', author='" + author + "', category='" + category + "'}";
+    }
+}
